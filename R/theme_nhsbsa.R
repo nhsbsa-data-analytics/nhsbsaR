6209a1eadc7a6bb0a27ab40062e36bd670b8f67a
adamivisonnhs <66741381+adamivisonnhs@users.noreply.github.com>
2021-09-27 12:27:42 +0100

    Fix font size bug

--- R/theme_nhsbsa.R
+++ R/theme_nhsbsa.R
@@ -49,7 +49,7 @@ theme_nhsbsa_gg <- function(plot, family = "sans") {
     text = ggplot2::element_text(
       color = "#231f20", 
       family = family, 
-      size = 12 / ggplot2::.pt
+      size = 12
     ),
 
     # Colour background NHS White
