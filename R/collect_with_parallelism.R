de5e4e6a723f0e136caf3627cbb828eb535ca80a
mgarbuzov <92371047+mgarbuzov@users.noreply.github.com>
2024-05-14 15:39:45 +0100

    Add compute_with_parallelist, update collect_with_parallelism

--- R/collect_with_parallelism.R
+++ R/collect_with_parallelism.R
@@ -1,6 +1,6 @@
 #' Collect a lazy table with specified degree of parallelism
 #'
-#' Execute and collect dbplyr lazy table with specified degree of parallelism. Adds a parallel hint to the first SELECT statement only: `SELECT /* +PARALLEL(N) */...` The hint is usually taken into account, but as it is only a hint, the database optimizer ultimately makes a final decision as to which degree of parallelism to use, if any. This is based on things like the query itself and the amount of concurrent use. To override the optimizer and force a specific degree, the session needs to be altered: `alter session enable parallel query; alter session force parallel query parallel n;` (not supported here).
+#' Execute and collect dbplyr lazy table with specified degree of parallelism. Adds a parallel hint to all SELECT statements: `SELECT /* +PARALLEL(N) */...` The hint is usually taken into account, but as it is only a hint, the database optimizer ultimately makes a final decision as to which degree of parallelism to use, if any. This is based on things like the query itself and the amount of concurrent use. To override the optimizer and force a specific degree, the session needs to be altered: `alter session enable parallel query; alter session force parallel query parallel n;` (not supported here).
 #'
 #' @param lazy_tbl A dbplyr lazy table.
 #'
@@ -21,13 +21,13 @@ collect_with_parallelism = function(lazy_tbl, n){
   # Specify degree of parallelism
   string_insert = paste0("SELECT /*+ PARALLEL(", n, ") */")
 
-  # Specify parallelism for first select
+  # Specify parallelism after each select
   query = gsub("SELECT", string_insert, sql_render(lazy_tbl))
 
   # Build new query
   new_query = dbplyr::build_sql(con = db_connection, query)
 
   # Collect newly generated sql
-  dplyr::tbl(src = db_connection, dplyr::sql(new_query)) %>% collect()
+  dplyr::tbl(src = db_connection, dplyr::sql(new_query)) |> collect()
 
 }
---
+++ R/compute_with_parallelism.R
@@ -0,0 +1,61 @@
+# Function to create table from query with specified degree of parallelism
+#'
+#'#' Compute (a-la dbplyr) a lazy table with specified degree of parallelism. Adds a parallel hint to all SELECT statements: `SELECT /* +PARALLEL(N) */...` The hint is usually taken into account, but as it is only a hint, the database optimizer ultimately makes a final decision as to which degree of parallelism to use, if any. This is based on things like the query itself and the amount of concurrent use.
+#'
+#' If hints are already present in any queries, e.g. by including them manually in parsing SQL using `dbplyr::sql()`, this function will overwrite them.
+#'
+#' @param lazy_tbl name of the dbplyr lazy table
+#' @param create_table_name name of user created table in SQL database
+#' @param n the degree of parallelism to enforce
+#' @param overwrite if `TRUE`, function will check if table exists, and if it does, will drop it; if `FALSE` will throw an error only if the table already exists.
+#'
+#' @examples
+#' \dontrun{
+#' compute_with_parallelism(table_db, "INT646_TABLE_DB", 32)
+#' }
+#' @export
+compute_with_parallelism = function(lazy_tbl, create_table_name, n, overwrite = F, materialize = F) {
+
+  # Pull the DB connection
+  db_connection <- lazy_tbl$src$con
+
+  # If overwrite is TRUE, check if the table exists, and if it does, drop it
+  if (overwrite == TRUE) {
+
+    if (DBI::dbExistsTable(
+          conn = db_connection,
+          name = DBI::Id(schema = toupper(db_connection@info$username), table = create_table_name)
+      ) == T
+     ) {
+      DBI::dbRemoveTable(
+        conn = db_connection,
+        name = DBI::Id(schema = toupper(db_connection@info$username), table = create_table_name)
+      )
+    }
+  }
+
+  if (materialize == T) {
+    string_insert = paste0("SELECT /*+ PARALLEL(", n, ") */")
+  } else {
+    string_insert = paste0("SELECT /*+ MATERIALIZE PARALLEL(", n, ") */")
+  }
+
+  # Specify parallelism after each select
+  new_query <- gsub(
+    "SELECT(\\s+/\\*.*\\*/)?",       # Replaces existing hints if they are present
+    string_insert,
+    dbplyr::sql_render(lazy_tbl),
+    ignore.case = T
+  )
+
+  new_query <- paste0(
+    "CREATE TABLE ",
+    create_table_name,
+    " NOLOGGING COMPRESS FOR QUERY HIGH AS ",
+    new_query
+  )
+
+  # Send query to the database
+  # (DBI::dbExecute returns the number of rows affected, which is silenced by assignment to 'output')
+  output <- DBI::dbExecute(conn = db_connection, statement = new_query)
+}
